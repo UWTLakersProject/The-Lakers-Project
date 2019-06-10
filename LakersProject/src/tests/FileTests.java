12c527c3b03df75fb158bbc30011682eec856cb9
yetti501 <chugrit85@gmail.com>
2019-06-10 14:59:17 -0700

    TestFile JUnit TEsting

---
+++ LakersProject/src/tests/FileTests.java
@@ -0,0 +1,21 @@
+//import static org.junit.*;
+package test;
+import guimain.ImportExportHelper;
+
+class FileTests {
+   ImportExportHelper testData;
+   
+   final void setup() {
+      testData = new ImportExportHelper();
+   }
+   
+   final void testUSerName(){
+      testData.setUserName("TESTNAME");
+      assertEquals("TESTNAME", testData.getUserName());
+   }
+   
+   final void testUserEamail(){
+      testData.setEmail("TEST@EMAIL.COM");
+      assertEquals("TEST@EMAIL.COM", testData.getEmail());
+   }
+}
