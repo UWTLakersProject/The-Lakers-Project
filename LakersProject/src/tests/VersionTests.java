a3fadd4d1908930be3443bba870fe8c0e5da0088
gargi79 <49700650+gargi79@users.noreply.github.com>
2019-06-13 04:57:37 -0700

    Update VersionTests.java

--- LakersProject/src/tests/VersionTests.java
+++ LakersProject/src/tests/VersionTests.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import guimain.DIYProjectMain;
 import guimain.Version;
-
+/**
+ * This class implements our jUnit test for our version class
+ * @author Daniel Loony, Garhgaj S
+ */
 
 //Sets and obtains the version automatically based off of the serialized version of the class
 //This class is helpful setting up our JUnit tests
